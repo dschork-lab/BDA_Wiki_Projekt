3b1acf30b266d180b6230f084dc352deee8decc2
Jonas Sperling <mail@jonas-sperling.de>
2021-11-14 15:08:24 +0100

    improved error handling to avoid crashes

--- src/WikipediaKafkaConnector.py
+++ src/WikipediaKafkaConnector.py
@@ -5,6 +5,7 @@ from kafka import KafkaProducer
 from kafka.errors import NoBrokersAvailable
 from sseclient import SSEClient
 from typing import Dict
+from datetime import datetime
 
 
 def create_producer_instance(bootstrap_server: str) -> KafkaProducer:
@@ -23,35 +24,48 @@ def create_producer_instance(bootstrap_server: str) -> KafkaProducer:
         exit(1)
 
 
-def reduce_event(change_event, old, new) -> Dict[str, str]:
+def check_for_category_response(response, page_id):
+    if "categories" not in response["query"]["pages"][f"{page_id}"].keys():
+        print(f"{datetime.now()} | response without categories parameter")
+        response["query"]["pages"][f"{page_id}"]["categories"] = []
+    return response
+
+
+def merge_event(change_event, old, new) -> Dict[str, str]:
     """
     :param change_event: Change event data
     :param old: Data of the old article version
     :param new: Data of the new article version
-    :return:
+    :return: formatted data to send to kafka topic
     """
-    page_id = list(old["query"]["pages"].keys())[0]
+    try:
+        page_id = list(old["query"]["pages"].keys())[0]
+        old = check_for_category_response(old, page_id)
+        new = check_for_category_response(new, page_id)
 
-    event = {
-        "id": change_event['id'],
-        "domain": change_event['meta']['domain'],
-        "timestamp": change_event['meta']['dt'],
-        "revision": {
-            "old": change_event['revision']['old'],
-            "new": change_event['revision']['new']
-        },
-        "old_version": {
-            "title": old["query"]["pages"][f"{page_id}"]["title"],
-            "content": old["query"]["pages"][f"{page_id}"]["extract"],
-            "categories": [x["title"][9:] for x in old["query"]["pages"][f"{page_id}"]["categories"]]
-        },
-        "new_version": {
-            "title": new["query"]["pages"][f"{page_id}"]["title"],
-            "content": new["query"]["pages"][f"{page_id}"]["extract"],
-            "categories": [x["title"][9:] for x in new["query"]["pages"][f"{page_id}"]["categories"]]
+        event = {
+            "id": change_event['id'],
+            "domain": change_event['meta']['domain'],
+            "timestamp": change_event['meta']['dt'],
+            "revision": {
+                "old": change_event['revision']['old'],
+                "new": change_event['revision']['new']
+            },
+            "old_version": {
+                "title": old["query"]["pages"][f"{page_id}"]["title"],
+                "content": old["query"]["pages"][f"{page_id}"]["extract"],
+                "categories": [x["title"][9:] for x in old["query"]["pages"][f"{page_id}"]["categories"]]
+            },
+            "new_version": {
+                "title": new["query"]["pages"][f"{page_id}"]["title"],
+                "content": new["query"]["pages"][f"{page_id}"]["extract"],
+                "categories": [x["title"][9:] for x in new["query"]["pages"][f"{page_id}"]["categories"]]
+            }
         }
-    }
-    return event
+        return event
+    except KeyError as e:
+        print(e)
+        pass
 
 
 if __name__ == "__main__":
@@ -77,5 +91,5 @@ if __name__ == "__main__":
                     if old_version.status_code == 200 and new_version.status_code == 200:
                         old_version_json = old_version.json()
                         new_version_json = new_version.json()
-                        reduced_event = reduce_event(event_data, old_version_json, new_version_json)
+                        reduced_event = merge_event(event_data, old_version_json, new_version_json)
                         producer.send("article_information", value=reduced_event)
